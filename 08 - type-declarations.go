33630c86a9d26b8db8c40c548cd022f01ef60239
Aldi Tegar Prakoso <alditegarprakoso@gmail.com>
2022-12-08 13:55:37 +0700

    add comment

--- 08 - type-declarations.go
+++ 08 - type-declarations.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	type NoKtp string // Menjadikan type alias NoKtp mennjadi type data string
-	type Married bool
+	type NoKtp string // Menjadikan type data alias NoKtp mennjadi type data string
+	type Married bool // Menjadikan type data alias Married mennjadi type data boolean (true or false)
 
 	var noKtpAldi NoKtp = "01293123129"
 	var marriedStatus Married = false
